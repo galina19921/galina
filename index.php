5def4a02bd7cb310108806d66618454404b21759
galina <gali4ka19921@gmail.com>
2021-10-08 10:52:17 +0300

    Test

--- index.php
+++ index.php
@@ -1,44 +1,178 @@
-1.Получить остаток деления 7 на 3
-$a = 7;
-$b = 3;
-echo ($a % $b);         //1
+<?php 
+/*1.посчитать длину массива 
+$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
+echo count($arr, COUNT_RECURSIVE);*/
 
-2.Получить целую часть сложения 7 и 7,15
-$a = 7;
-$b = 7.15;
-echo intval($a + $b);    //14
+//2.переместить первые 4 элемента массива в конец массива
+/*$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
+$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
+$v = $arr[0];
+$p = $arr[1];
+$o = $arr[2];
+$t = $arr[3];
+unset($arr[0], $arr[1],  $arr[2],  $arr[3]);   //Array ( [4] => 31 [5] => 4 [6] => 1 [7] => 8 [8] => 6 [0] => 1 [1] => 2 [2] => 3 [3] => 7 )
+$arr[0]=$v;
+$arr[1]=$p;
+$arr[2]=$o;
+$arr[3]=$t;
+print_r($arr);*/
 
-3.Получить корень из 25
-$a=25;
-echo sqrt($a);       //5
+/*3.получить сумму 4,5,6 элемента
+$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
+$my_arr = array_sum(array_slice($arr, 3, 3));
+echo $my_arr;                                 //42
+*/
 
-4.Получить 4-е слово из фразы - Десять негритят пошли купаться в море
-$phrase = 'Десять негритят пошли купаться в море';
-$fourth_word = explode(' ', trim($phrase)) [3];
-echo $fourth_word;                 //купаться
 
-5.Получить 17-й символ из фразы - Десять негритят пошли купаться в море
-$phrase = 'Десять негритят пошли купаться в море';
-echo mb_substr($phrase, 16, 1, 'UTF-8');     //п
+/*4. найти все элементы которые отсутствуют в первом массиве и присутствуют во втором
+$firstArr = [
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+  'foure' => 5,
+  'five' => 12,
+];
 
-6.Сделать заглавной первую букву во всех словах фразы - Десять негритят пошли купаться в море
-$phrase = "Десять негритят пошли купаться в море";
-echo mb_convert_case($phrase, MB_CASE_TITLE, 'UTF-8');   //Десять Негритят Пошли Купаться В Море
+$secondArr = [
+  'one' => 1,
+  'seven' => 22,
+  'three' => 32,
+  'foure' => 5,
+  'five' => 13,
+  'six' => 37,
+];
+var_dump(array_diff_key($secondArr, $firstArr)); //array(2) { ["seven"]=> int(22) ["six"]=> int(37) } }*/
 
-7.Посчитать длину строки - Десять негритят пошли купаться в море
-$phrase = "Десять негритят пошли купаться в море";
-echo mb_strlen($phrase);                              //37
+//5. найти все элементы которые присутствую в первом и отсутствуют во втором
+/*$firstArr = [
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+  'foure' => 5,
+  'five' => 12,
+];
 
-8.Правильно ли утверждение true равно 1
-var_dump(true == 1);                  // bool(true)
+$secondArr = [
+  'one' => 1,
+  'seven' => 22,
+  'three' => 32,
+  'foure' => 5,
+  'five' => 13,
+  'six' => 37,
+];
+var_dump(array_diff_key($firstArr, $secondArr));*/  //array(1) { ["two"]=> int(2) }
 
-9.Правильно ли утверждение false тождественно 0
-var_dump(false === 0);                //bool(true)
+//6. найти все элементы значения которых совпадают 
+/*$firstArr = [
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+  'foure' => 5,
+  'five' => 12,
+];
 
-10. Какая строка длиннее three - три
-var_dump('tree' > 'три');             //bool(false)
+$secondArr = [
+  'one' => 1,
+  'seven' => 22,
+  'three' => 32,
+  'foure' => 5,
+  'five' => 13,
+  'six' => 37,
+];
+var_dump (array_intersect($firstArr, $secondArr)); //array(2) { ["one"]=> int(1) ["foure"]=> int(5) } */
 
-11.Какое число больше 125 умножить на 13 плюс 7 или 223 плюс 28 умножить 2  
-var_dump((125*13+7) > (223+28*2));       //bool(true)
+/*7.найти все элементы значения которых отличается ///////////////////////////////////////
+$firstArr = [
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+  'foure' => 5,
+  'five' => 12,
+];
+
+$secondArr = [
+  'one' => 1,
+  'seven' => 22,
+  'three' => 32,
+  'foure' => 5,
+  'five' => 13,
+  'six' => 37,
+];
+var_dump ( array_diff($firstArr, $secondArr));
+var_dump ( array_diff($secondArr, $firstArr)); */ // array(3) { ["two"]=> int(2) ["three"]=> int(3) ["five"]=> int(12) }
+
+/*8.получить все вторые элементы вложенных массивов/////////////////////////////
+$firstArr = [
+  'one' => 1,
+  'two' => ['one' => 1, 'seven' => 22, 'three' => 32,],
+  'three' => ['one' => 1, 'two' => 2,],
+  'foure' => 5,
+  'five' => ['three' => 32, 'foure' => 5,'five' => 12,],  
+];
+не зробила
+};*/
+
+// массив $arr сейчас таков: array(2, 4, 6, 8)
+//unset($value); 
+
+
+
+	
+/*$array = [
+    [1, 2],
+    [3, 4],
+];
+foreach ($array as list($a)) {
+    // Обратите внимание на отсутствие $b.
+    echo "$a\n";
+}
+*/
+ 
+/*9. получить общее количество элементов в массиве
+$firstArr = [
+  'one' => 1,
+  'two' => [
+  'one' => 1,
+  'seven' => 22,
+  'three' => 32,
+  ],
+  'three' => [
+  'one' => 1,
+  'two' => 2,
+  ],
+  'foure' => 5,
+  'five' => [
+  'three' => 32,
+  'foure' => 5,
+  'five' => 12,
+],  
+];
+
+var_dump(count( $firstArr, COUNT_RECURSIVE));*/    //int(13) 
+
+ /*10. получить сумму всех значений в массиве
+ $firstArr = [
+  'one' => 1,
+  'two' => ['one' => 1, 'seven' => 22, 'three' => 32,],
+  'three' => ['one' => 1, 'two' => 2,],
+  'foure' => 5,
+  'five' => ['three' => 32, 'foure' => 5,'five' => 12,],  
+];
+function sumArr( $firstArr) {
+    $sum = 0;
+    foreach( $firstArr as $v) {
+        if (is_array($v)) {
+            $sum += sumArr($v);
+        } else {
+            $sum += $v;
+        }
+    }
+    return $sum;
+}
+echo sumArr( $firstArr);    */  //////    113
+
+
+
+
 
 
